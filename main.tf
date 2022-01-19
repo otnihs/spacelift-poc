ae41e8071ec1c265bd1d5fe8c46773914fb42b22
gunnerzapp <gunnerzapp@gmail.com>
2022-01-19 15:51:51 +0530

    region

--- main.tf
+++ main.tf
@@ -63,4 +63,5 @@ tSLN5kq1XLf0DywcLagV0I0m8DYEEfs=
   worker_pool_id    = "01FSRVG2ZXHK5KE9BXY5HZ6YY7"
   security_groups   = ["sg-af4fbfa4"]
   vpc_subnets       = ["subnet-26ec9a7b", "subnet-65c7472f"]
+  region            = "us-west-2"
 }
